45b549901e6560f39b448cb518d2e6c318e8bd78
David Gelpi <gelpiorama@gmail.com>
2025-02-05 01:50:37 +0100

    refactor(A*): Eliminar loops

--- src/collection.py
+++ src/collection.py
@@ -1,4 +1,5 @@
 class Set:
+
     def __init__(self):
         self.elements = set()
 
@@ -9,16 +10,13 @@ class Set:
         self.elements.discard(node)
 
     def contains(self, node):
-        for element in self.elements:
-            if element == node:
-                return True
-        return False
+        return any(element == node for element in self.elements)
 
     def is_empty(self):
-        return len(self.elements) == 0
+        return self.size() == 0
 
     def size(self):
-        return len(self.elements)
+        return len(self.get_elements())
 
     def get_elements(self):
         return self.elements
