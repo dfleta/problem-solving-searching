43c752b6d3cf9000ee36fa14bd6401c147c302ed
David Gelpi <gelpiorama@gmail.com>
2025-02-11 18:49:56 +0100

    refactor(output): Eliminar codigo colores redundante

--- src/cli_colors.py
+++ src/cli_colors.py
@@ -4,9 +4,9 @@ from enum import Enum
 class Colors(Enum):
     BLUE = "\033[94m"
     GREEN = "\033[92m"
-    RED = "\033[1;31;40m"
-    YELLOW = "\033[1;33;40m"
-    PURPLE = "\033[1;35;40m"
+    RED = "\033[31m"
+    YELLOW = "\033[33m"
+    PURPLE = "\033[35m"
     ENDC = "\033[0m"
 
     def __str__(self):
