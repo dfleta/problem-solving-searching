65a8899821688232738934eddecef27c93e3a100
David Gelpi <gelpiorama@gmail.com>
2026-01-12 14:18:09 +0100

    refactor(cli_colors): Modificar codigo de colores

--- src/output.py
+++ src/output.py
@@ -10,9 +10,9 @@ def problem_repr(solution, explored, frontier, initial_state, goal_state):
             if state == initial_state:
                 _show_state_colored(state, Colors.RED)
             elif state == goal_state:
-                _show_state_colored(state, Colors.YELLOW)
+                _show_state_colored(state, Colors.BRIGHT_YELLOW)
             elif state in solution:
-                _show_state_colored(state, Colors.GREEN)
+                _show_state_colored(state, Colors.BRIGHT_GREEN)
             elif state in str(explored.get_elements()):
                 _show_state_colored(state, Colors.BLUE)
             elif state in str(frontier.get_elements()):
@@ -23,9 +23,9 @@ def problem_repr(solution, explored, frontier, initial_state, goal_state):
     print("\nStart state:", end=" ")
     _show_state_colored(initial_state, Colors.RED)
     print("\nGoal State:", end=" ")
-    _show_state_colored(goal_state, Colors.YELLOW)
+    _show_state_colored(goal_state, Colors.BRIGHT_YELLOW)
     print("\nSolution: ")
-    _show_state_colored(solution, Colors.GREEN)
+    _show_state_colored(solution, Colors.BRIGHT_GREEN)
     print("\nExplored (state g): ")
     _show_state_colored(explored.get_elements(), Colors.BLUE)
     print("\nFrontier: ")
@@ -34,4 +34,4 @@ def problem_repr(solution, explored, frontier, initial_state, goal_state):
 
 
 def _show_state_colored(state, color):
-    print(f"{color}{state}{Colors.ENDC}", end=" ")
+    print(f"{color}{state}{Colors.RESET}", end=" ")
